61655317622925621de4b649f04215054abda975
seregapashchenko97-cmd <seregapashchenko97@gmail.com>
2026-06-01 20:57:55 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,30 @@
+import aiosqlite
 
+
+DB_NAME = "bot.db"
+
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS channels (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            channel_id INTEGER,
+            title TEXT
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            channel_id INTEGER,
+            text TEXT,
+            publish_at TEXT,
+            status TEXT DEFAULT 'pending'
+        )
+        """)
+
+        await db.commit()
