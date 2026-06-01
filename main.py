e0bf3a2979181999f20f2b5beb877836e1754526
seregapashchenko97-cmd <seregapashchenko97@gmail.com>
2026-06-01 17:35:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import asyncio
+import os
 
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Бот запущен 🚀")
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
